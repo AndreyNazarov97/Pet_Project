dd411d3a4f970baadcade0a25dc4b3f54fffe54e
AndreyNazarov97 <gas4556@mail.ru>
2024-08-08 14:44:26 +0300

    Create Volunteer model

---
+++ Backend/src/PetProject.Domain/Entities/Volunteer.cs
@@ -0,0 +1,21 @@
+﻿namespace PetProject.Domain.Entities;
+
+public class Volunteer
+{
+    public Guid Id { get; }
+    
+    public string FullName { get; }
+    
+    public string Description { get; }
+    
+    public int Experience { get; }
+    
+    public int PetsAdopted { get; }
+    
+    public string PhoneNumber { get; }
+    
+    public List<string> SocialNetworks { get; }
+    
+    public List<Requisite> Requisites { get; }
+    
+}
