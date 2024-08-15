137aa0e18e5097aed53252c434207b7bd73850cf
AndreyNazarov97 <gas4556@mail.ru>
2024-08-15 09:10:42 +0300

    add migration

---
+++ Backend/src/PetProject.Infrastructure.Postgres/Migrations/20240814192841_Initial.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetProject.Infrastructure.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "species",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_species", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    experience = table.Column<int>(type: "integer", nullable: false),
+                    pets_adopted = table.Column<int>(type: "integer", nullable: false),
+                    pets_found_home_quantity = table.Column<int>(type: "integer", nullable: false),
+                    pets_in_treatment = table.Column<int>(type: "integer", nullable: false),
+                    full_name_first_name = table.Column<string>(type: "text", nullable: false),
+                    full_name_last_name = table.Column<string>(type: "text", nullable: false),
+                    full_name_patronymic = table.Column<string>(type: "text", nullable: true),
+                    phone_number_number = table.Column<string>(type: "text", nullable: false),
+                    requisites = table.Column<string>(type: "jsonb", nullable: true),
+                    social_networks = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_volunteers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "breeds",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_breeds", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_breeds_species_species_id",
+                        column: x => x.species_id,
+                        principalTable: "species",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pets",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    breed_name = table.Column<string>(type: "text", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: false),
+                    health_info = table.Column<string>(type: "text", nullable: false),
+                    weight = table.Column<double>(type: "double precision", nullable: false),
+                    height = table.Column<double>(type: "double precision", nullable: false),
+                    is_castrated = table.Column<bool>(type: "boolean", nullable: false),
+                    birth_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    is_vaccinated = table.Column<bool>(type: "boolean", nullable: false),
+                    help_status = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    address_city = table.Column<string>(type: "text", nullable: false),
+                    address_country = table.Column<string>(type: "text", nullable: false),
+                    address_flat = table.Column<string>(type: "text", nullable: false),
+                    address_house = table.Column<string>(type: "text", nullable: false),
+                    address_street = table.Column<string>(type: "text", nullable: false),
+                    owner_phone_number_number = table.Column<string>(type: "text", nullable: false),
+                    requisites = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_pets", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_pets_species_species_id",
+                        column: x => x.species_id,
+                        principalTable: "species",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pet_photos",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    path = table.Column<string>(type: "text", nullable: false),
+                    is_main = table.Column<bool>(type: "boolean", nullable: false),
+                    pet_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_pet_photos", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_pet_photos_pets_pet_id",
+                        column: x => x.pet_id,
+                        principalTable: "pets",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_breeds_species_id",
+                table: "breeds",
+                column: "species_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_pet_photos_pet_id",
+                table: "pet_photos",
+                column: "pet_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_pets_species_id",
+                table: "pets",
+                column: "species_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "breeds");
+
+            migrationBuilder.DropTable(
+                name: "pet_photos");
+
+            migrationBuilder.DropTable(
+                name: "volunteers");
+
+            migrationBuilder.DropTable(
+                name: "pets");
+
+            migrationBuilder.DropTable(
+                name: "species");
+        }
+    }
+}
