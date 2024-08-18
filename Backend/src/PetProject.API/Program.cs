bf2bbe411e31c650847e7d70da20342524dc350c
AndreyNazarov97 <gas4556@mail.ru>
2024-08-18 22:53:31 +0300

    add IOptionsPattern

--- Backend/src/PetProject.API/Program.cs
+++ Backend/src/PetProject.API/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.Extensions.Options;
+using Minio;
 using Minio.AspNetCore;
 using PetProject.API.Middlewares;
 using PetProject.API.Providers;
 using PetProject.Application;
 using PetProject.Application.Abstractions;
 using PetProject.Infrastructure.Postgres;
+using MinioOptions = PetProject.API.Providers.MinioOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,19 +16,21 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddMinio(options =>
 {
-    options.Endpoint = builder.Configuration.GetValue<string>("Minio:Endpoint");
-    options.AccessKey = builder.Configuration.GetValue<string>("Minio:AccessKey");
-    options.SecretKey = builder.Configuration.GetValue<string>("Minio:SecretKey");
+    var minioOptions = builder.Configuration.GetSection("Minio").Get<MinioOptions>();
+    options.AccessKey = minioOptions!.AccessKey;
+    options.Endpoint = minioOptions.Endpoint;
+    options.SecretKey = minioOptions.SecretKey;
 });
+
 
 builder.Services.AddApplication();
 builder.Services.AddPostgresInfrastructure(builder.Configuration);
-builder.Services.AddSingleton<IMinioProvider, MinioProvider>();
+builder.Services.AddScoped<IMinioProvider, MinioProvider>();
 
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+//app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Backend/src/PetProject.API/Providers/MinioOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace PetProject.API.Providers;
+
+public class MinioOptions
+{
+    public string Endpoint { get; init; }
+    public string AccessKey { get; init; }
+    public string SecretKey { get; init; }
+}
