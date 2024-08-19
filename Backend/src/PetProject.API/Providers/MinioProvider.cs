660299a16d1a808ab460fb57e94cf4288e172351
AndreyNazarov97 <gas4556@mail.ru>
2024-08-19 11:24:42 +0300

    Add logging to MinioProvider

--- Backend/src/PetProject.API/Providers/MinioProvider.cs
+++ Backend/src/PetProject.API/Providers/MinioProvider.cs
@@ -2,6 +2,8 @@
 using Minio.DataModel.Args;
 using PetProject.Application.Abstractions;
 using PetProject.Domain.Shared;
+using ILogger = Serilog.ILogger;
+
 
 namespace PetProject.API.Providers;
 
@@ -9,10 +11,14 @@ public class MinioProvider : IMinioProvider
 {
     private const int EXPIRY = 604800; // 7 days
     private readonly IMinioClient _minioClient;
+    private readonly ILogger _logger;
 
-    public MinioProvider(IMinioClient minioClient)
+    public MinioProvider(
+        IMinioClient minioClient,
+        ILogger logger)
     {
         _minioClient = minioClient;
+        _logger = logger;
     }
 
     public async Task<Result<string>> DownloadFile(string bucketName, string fileName)
@@ -23,8 +29,12 @@ public class MinioProvider : IMinioProvider
                 .WithBucket(bucketName)
                 .WithObject(fileName)
                 .WithExpiry(EXPIRY);
-
+            
+            _logger.Information("File {FileName} downloading from Minio", fileName);
+            
             var downloadUrl = await _minioClient.PresignedGetObjectAsync(getObjectArgs);
+            
+            _logger.Information("File {FileName} downloaded from Minio with url {DownloadUrl}", fileName, downloadUrl);
             return downloadUrl;
         }
         catch (Exception e)
@@ -55,7 +65,11 @@ public class MinioProvider : IMinioProvider
                 .WithObjectSize(stream.Length)
                 .WithContentType("application/octet-stream");
 
+            _logger.Information("File {FileName} uploading to Minio", fileName);
+            
             await _minioClient.PutObjectAsync(putObjectArgs, cancellationToken);
+            
+            _logger.Information("File {FileName} uploaded to Minio", fileName);
 
             return Result.Success();
         }
@@ -72,8 +86,12 @@ public class MinioProvider : IMinioProvider
             var removeObjectArgs = new RemoveObjectArgs()
                 .WithBucket(bucketName)
                 .WithObject(fileName);
+            
+            _logger.Information("File {FileName} deleting from Minio", fileName);
+            
             await _minioClient.RemoveObjectAsync(removeObjectArgs, cancellationToken);
-
+            
+            _logger.Information("File {FileName} deleted from Minio", fileName);
             return Result.Success();
         }
         catch (Exception e)
