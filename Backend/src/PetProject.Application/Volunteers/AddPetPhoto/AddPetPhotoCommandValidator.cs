a1d6349f94f36a2bdbe68880b36fac8a4b7f497b
AndreyNazarov97 <gas4556@mail.ru>
2024-11-15 14:49:09 +0300

    add validators

---
+++ Backend/src/PetProject.Application/Volunteers/AddPetPhoto/AddPetPhotoCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using PetProject.Application.Validation;
+using PetProject.Domain.Shared;
+
+namespace PetProject.Application.Volunteers.AddPetPhoto;
+
+public class AddPetPhotoCommandValidator : AbstractValidator<AddPetPhotoCommand>
+{
+    public AddPetPhotoCommandValidator()
+    {
+        RuleFor(p => p.PetId)
+            .NotEmpty().WithError(Errors.General.ValueIsRequired("PetId"));
+        
+        RuleFor(p => p.VolunteerId)
+            .NotEmpty().WithError(Errors.General.ValueIsRequired("VolunteerId"));
+        
+        RuleForEach(p => p.Photos)
+            .ChildRules(p =>
+            {
+                p.RuleFor(p => p.FileName)
+                    .Must(ext => Constants.Extensions.Contains(Path.GetExtension(ext)))
+                    .NotEmpty().WithError(Errors.General.ValueIsInvalid("FileName"));
+
+                p.RuleFor(p => p.Content)
+                    .Must(s => s.Length is > 0 and <= 15 * 1024 * 1024).WithError(Errors.General.ValueIsInvalid("Content"));
+            });
+
+    }
+}
---
+++ Backend/src/PetProject.Application/Volunteers/CreatePet/CreatePetCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using PetProject.Application.Validation;
+using PetProject.Domain.Shared.ValueObjects;
+using PetProject.Domain.VolunteerManagement.ValueObjects;
+
+namespace PetProject.Application.Volunteers.CreatePet;
+
+public class CreatePetCommandValidator : AbstractValidator<CreatePetCommand>
+{
+    public CreatePetCommandValidator()
+    {
+        RuleFor(c => new { c.Address.Country, c.Address.City, c.Address.Street, c.Address.House, c.Address.Flat })
+            .MustBeValueObject(x => Address.Create(x.Country, x.City, x.Street, x.House, x.Flat));
+
+        RuleFor(c => c.Name)
+            .MustBeValueObject(PetName.Create);
+
+        RuleFor(c => c.GeneralDescription)
+            .MustBeValueObject(Description.Create);
+
+        RuleFor(c => c.HealthInformation)
+            .MustBeValueObject(Description.Create);
+
+        RuleFor(c => c.Species)
+            .MaximumLength(100);
+
+        RuleFor(c => c.Breed)
+            .MaximumLength(100);
+
+        RuleFor(c => new{c.Height, c.Weight})
+            .MustBeValueObject(x=> PetPhysicalAttributes.Create(x.Weight, x.Height));
+        
+        RuleFor(c => c.BirthDate)
+            .Must(birthDate => birthDate.ToDateTime(TimeOnly.MinValue) < DateTime.UtcNow)
+            .WithMessage("Birth date must be in the past");
+    }
+}
