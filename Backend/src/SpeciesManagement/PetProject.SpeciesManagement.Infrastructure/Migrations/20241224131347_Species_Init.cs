b529584e7c749162cbf4ff3b09f0afd228ef81b2
Andrew Nazarov <gas4556@mail.ru>
2024-12-24 16:17:12 +0300

    migrations

---
+++ Backend/src/SpeciesManagement/PetProject.SpeciesManagement.Infrastructure/Migrations/20241224131347_Species_Init.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetProject.SpeciesManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Species_Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "species");
+
+            migrationBuilder.CreateTable(
+                name: "species",
+                schema: "species",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    species_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deletion_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_species", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "breeds",
+                schema: "species",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    breed_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deletion_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_breeds", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_breeds_species_species_id",
+                        column: x => x.species_id,
+                        principalSchema: "species",
+                        principalTable: "species",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_breeds_species_id",
+                schema: "species",
+                table: "breeds",
+                column: "species_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "breeds",
+                schema: "species");
+
+            migrationBuilder.DropTable(
+                name: "species",
+                schema: "species");
+        }
+    }
+}
---
+++ Backend/src/VolunteerManagement/PetProject.VolunteerManagement.Infrastructure/Migrations/20241224131342_Volunteers_Init.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetProject.VolunteerManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Volunteers_Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "volunteers");
+
+            migrationBuilder.CreateTable(
+                name: "volunteers",
+                schema: "volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    age_experience = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    patronymic = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    general_description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deletion_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_volunteers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pets",
+                schema: "volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    birth_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    is_castrated = table.Column<bool>(type: "boolean", nullable: false),
+                    is_vaccinated = table.Column<bool>(type: "boolean", nullable: false),
+                    help_status = table.Column<string>(type: "text", nullable: false),
+                    date_created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    pet_photos = table.Column<string>(type: "jsonb", nullable: false),
+                    volunteer_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    city = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    country = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    flat = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    house = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    street = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    breed_name = table.Column<string>(type: "text", nullable: false),
+                    species_name = table.Column<string>(type: "text", nullable: false),
+                    general_description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    health_information = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    pet_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    height = table.Column<double>(type: "double precision", nullable: false),
+                    weight = table.Column<double>(type: "double precision", nullable: false),
+                    position = table.Column<int>(type: "integer", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deletion_date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_pets", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_pets_volunteers_volunteer_id",
+                        column: x => x.volunteer_id,
+                        principalSchema: "volunteers",
+                        principalTable: "volunteers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_pets_volunteer_id",
+                schema: "volunteers",
+                table: "pets",
+                column: "volunteer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pets",
+                schema: "volunteers");
+
+            migrationBuilder.DropTable(
+                name: "volunteers",
+                schema: "volunteers");
+        }
+    }
+}
